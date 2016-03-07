ad8e43a37562374150a3fd633a62055c1471c855
jfambro <Jonathan.Fambro@careerbuilder.com>
2016-03-06 21:44:02 -0500

    Created a branch and code

---
+++ exercise1.rb
@@ -0,0 +1,12 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+#Exercise1
+puts "puts command prints the statement and adds a line"
+print "prints command on print the statement only"
+print "# comment display comments about the code but not for the end-user to see"
